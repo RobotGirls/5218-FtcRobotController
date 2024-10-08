91f59d16a09919b645f7da37c9e13b4bbdc5ecd3
Madelyn <madelyntran@rocknrollrobots25.com>
2024-10-07 18:23:57 -0700

    Began the code for RRTeleop

---
+++ TeamCode/src/main/java/RRTeleop.java
@@ -0,0 +1,44 @@
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RRTeleop extends OpMode {
+    private FtcDashboard dash = FtcDashboard.getInstance();
+    private List<Action> runningActions = new ArrayList<>();
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void loop() {
+        TelemetryPacket packet = new TelemetryPacket();
+
+        // updated based on gamepads
+
+        // update running actions
+        List<Action> newActions = new ArrayList<>();
+        for (Action action : runningActions) {
+            action.preview(packet.fieldOverlay());
+            if (action.run(packet)) {
+                newActions.add(action);
+            }
+//            if (gamepad1.a) {
+//                runningActions.add(new SequentialAction(
+//                        new SleepAction(0.5),
+//                        new InstantAction(() -> servo.setPosition(0.5))
+//                ));
+//            }
+        }
+        runningActions = newActions;
+
+        dash.sendTelemetryPacket(packet);
+    }
+}
