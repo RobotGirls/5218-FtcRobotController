f890d7be4e3a82eb3eab22fa77116c6b79fa9e74
Elyse <elyseluo@rocknrollrobots25>
2024-11-09 14:53:01 -0800

    changed the axialGain, headingGain, lateralGain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -83,8 +83,8 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
+        public double axialGain = 1.75;
+        public double lateralGain = 1;
         public double headingGain = 0.0; // shared with turn
 
         public double axialVelGain = 0.0;
