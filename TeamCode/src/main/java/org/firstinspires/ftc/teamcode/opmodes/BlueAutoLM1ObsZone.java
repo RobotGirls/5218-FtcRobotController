635c67bc2120950be2b6de6e7beed20ca8f3f085
Elyse <elyseluo@rocknrollrobots25>
2024-11-11 20:01:12 -0800

    created the auto for LM1 that goes to the subermisble, goes backwards, and then parks in the observation zone. Tested and works, no lift yet.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BlueAutoLM1ObsZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BlueAutoLM1ObsZone.java
@@ -4,8 +4,8 @@ import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
-import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -18,9 +18,20 @@ public class BlueAutoLM1ObsZone extends LinearOpMode {
         Pose2d initialPose = new Pose2d(0, 0, Math.toRadians(90));
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
         TrajectoryActionBuilder toSubmersible = drive.actionBuilder(initialPose)
-                .turn(45)
+                //.turn(Math.toRadians(45))
+                .strafeTo(new Vector2d(-20,29))
                 .waitSeconds(2);
         Action toSubmersibleTraj = toSubmersible.build();
+
+        Action toObservation = toSubmersible.fresh()
+                .lineToY(-5)
+                .waitSeconds(1)
+                .setTangent(Math.toRadians(0))
+                .lineToX(25)
+                .waitSeconds(2)
+              //  .setTangent(0)
+                .build();
+
 
         while (!isStopRequested() && !opModeIsActive()) {
             telemetry.addData("Robot position: ", drive.updatePoseEstimate());
@@ -30,7 +41,8 @@ public class BlueAutoLM1ObsZone extends LinearOpMode {
 
         Actions.runBlocking(
                 new SequentialAction(
-                        toSubmersibleTraj
+                       toSubmersibleTraj,
+                       toObservation
                 )
         );
 
