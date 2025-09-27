939fe01b69f3da6d72076eb7c0fec24ab4fdb11b
Isabella Barrientos <isabellabarrientos@rocknrollrobots25.com>
2025-09-27 12:05:29 -0700

    This is for the amming of april tag and how fast the motor will go

---
+++ TeamCode/src/main/java/AprilTagSpeedMotor.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.hardware.dfrobot.HuskyLens;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+
+import java.util.concurrent.TimeUnit;
+
+@TeleOp(name = "HuskyLens with Motor", group = "Sensor")
+//@Disabled
+public class AprilTagSpeedMotor extends LinearOpMode {
+
+    private final int READ_PERIOD = 1;
+    private HuskyLens huskyLens;
+    private DcMotor driveMotor;
+
+    @Override
+    public void runOpMode() {
+        huskyLens = hardwareMap.get(HuskyLens.class, "huskylens");
+        driveMotor = hardwareMap.get(DcMotor.class, "TestingMotor"); // configure this in Robot Config
+        driveMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        driveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        Deadline rateLimit = new Deadline(READ_PERIOD, TimeUnit.SECONDS);
+        rateLimit.expire();
+
+        if (!huskyLens.knock()) {
+            telemetry.addData(">>", "Problem communicating with " + huskyLens.getDeviceName());
+        } else {
+            telemetry.addData(">>", "Press start to continue");
+        }
+
+        // Look for AprilTags
+        huskyLens.selectAlgorithm(HuskyLens.Algorithm.TAG_RECOGNITION);
+
+        telemetry.update();
+        waitForStart();
+
+        while(opModeIsActive()) {
+            if (!rateLimit.hasExpired()) {
+                continue;
+            }
+            rateLimit.reset();
+
+            HuskyLens.Block[] blocks = huskyLens.blocks();
+            telemetry.addData("Block count", blocks.length);
+
+            if (blocks.length > 0) {
+                // Use first detected tag
+                int tagWidthPx = blocks[0].width;
+
+                if (tagWidthPx > 0) {
+                    // Estimate distance (inverse of width in pixels)
+                    double distanceEstimate = 1.0 / tagWidthPx;
+
+                    // Stop threshold: if close enough, stop
+                    double stopThreshold = 0.01; // tune this for your setup
+
+                    double motorPower = 0.0;
+                    if (distanceEstimate > stopThreshold) {
+                        // Farther → faster
+                        double k = 50.0; // scaling constant, tune experimentally
+                        motorPower = k * distanceEstimate;
+                        motorPower = Math.min(Math.max(motorPower, 0.0), 1.0);
+                    }
+
+                    driveMotor.setPower(motorPower);
+
+                    telemetry.addData("Tag Width (px)", tagWidthPx);
+                    telemetry.addData("Estimated Distance", distanceEstimate);
+                    telemetry.addData("Motor Power", motorPower);
+                }
+            } else {
+                driveMotor.setPower(0); // stop if no tag detected
+            }
+
+            telemetry.update();
+        }
+    }
+}
