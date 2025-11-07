4322509c3ddfa3d0bd623229f32835d6ff9660fa
unknown <sabrinaschneider@rocknrollrobots25.com>
2025-11-06 20:18:12 -0800

    Changed code to find IMU

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -77,6 +77,8 @@ public final class MecanumDrive {
         public double minProfileAccel = -30;
         public double maxProfileAccel = 50;
 
+
+
         // turn profile parameters (in radians)
         public double maxAngVel = Math.PI; // shared with path
         public double maxAngAccel = Math.PI;
