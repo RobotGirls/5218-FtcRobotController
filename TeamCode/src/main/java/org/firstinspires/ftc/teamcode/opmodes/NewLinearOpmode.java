56541e93fc38b9d15a5bcc2047f2a5dd3f9f2c3a
Madelyn <madelyntran@rocknrollrobots25.com>
2025-11-11 22:51:26 -0800

    adjusted motor powers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/NewLinearOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/NewLinearOpmode.java
@@ -46,9 +46,9 @@ public class NewLinearOpmode extends LinearOpMode {
         rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
-       // rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+ // rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+//       leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+//     //   leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
         // Intake
@@ -80,9 +80,9 @@ public class NewLinearOpmode extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive() && !isStopRequested()) {
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
+            double y = -gamepad1.right_stick_x; // Remember, Y stick value is reversed
+            double x = gamepad1.right_stick_y * 1.1; // Counteract imperfect strafing
+            double rx = -gamepad1.left_stick_x;
 
             // double FlywheelPower = gamepad2.right_stick_y; // example
             // FlywheelMotor.setPower(FlywheelPower);
@@ -121,8 +121,8 @@ public class NewLinearOpmode extends LinearOpMode {
             // but only if at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
+            double backLeftPower = (y - x - rx) / denominator;
+            double frontRightPower = (y - x + rx) / denominator;
             double backRightPower = (y + x - rx) / denominator;
 
 
